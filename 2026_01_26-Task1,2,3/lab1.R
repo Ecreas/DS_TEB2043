93041adf815cc49cf79b2a04b51f67f00da2b8ec
Ecreas <140377885+Ecreas@users.noreply.github.com>
2026-01-26 11:34:12 +0800

    Add files via upload

---
+++ 2026_01_26-Task1,2,3/lab1.R
@@ -0,0 +1,11 @@
+#24006841 Najmi
+
+a <- seq(from = 1, to = 20)^2
+
+num1 = round(0.956786, 2)
+num2 = round(7.8345901, 3)
+
+
+r <- readline(prompt= "Enter radius: ")
+r <- as.numeric(r)
+area = 3.142 * (r^2)
