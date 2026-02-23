53c4b73b122772d8834a0b6e94bc0038abfd5416
Ecreas <140377885+Ecreas@users.noreply.github.com>
2026-02-23 11:15:10 +0800

    Add files via upload

---
+++ 2026_02_23-Lab6/Lab6_Task4.R
@@ -0,0 +1,32 @@
+student_scores = data.frame(
+  
+  names = c("Anastasia", "Dima", "Michael", "Matthew", "Laura", "Kevin", "Jonas"),
+  score = c(12.5, 9.0, 16.5, 12.0, 9.0, 8.0, 19.0),
+  attempts = c(1,3,2,3,2,1,2)
+  
+)
+
+print(student_scores)
+
+student_scores$qualify <- c("yes","no", "yes", "no", "no", "no", "yes")
+
+print(student_scores)
+
+new_row = data.frame(
+  
+  names = "Emily",
+  score = 14.5,
+  attempts = 1,
+  qualify = "yes"
+  
+)
+
+new_student_scores = rbind(student_scores, new_row)
+
+print(new_student_scores)
+
+str(new_student_scores)
+summary(new_student_scores)
+cat("number of rows: ", nrow(new_student_scores))
+cat("number of columns: ", ncol(new_student_scores))
+
