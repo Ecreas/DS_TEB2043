d53ca99a7381cebcf33b28c6c2665581e63e0bdb
Ecreas <140377885+Ecreas@users.noreply.github.com>
2026-03-02 11:11:32 +0800

    Add files via upload

---
+++ 2026_03_02-Lab7/Lab7_Task1.R
@@ -0,0 +1,37 @@
+library(readxl)
+library(dplyr)
+
+titanic <- read.csv("titanic.csv") 
+View(titanic)
+
+
+sum(is.na(titanic))
+which(is.na(titanic))
+
+
+print(sapply(titanic, function(x) sum(is.na(x)))) 
+
+dim(titanic)
+titanic_cleaned = na.omit(titanic)
+View (titanic_cleaned)
+
+colnames(titanic_cleaned)
+
+
+print(filter(titanic_cleaned,Sex == 'female')) 
+print(filter(titanic_cleaned,Fare > 500)) 
+
+print(filter(titanic_cleaned,Sex == 'female' & Fare > 500)) 
+
+
+titanic_sortbyfare = arrange(titanic_cleaned, Fare) 
+View(titanic_sortbyfare)
+
+
+totalSurvived <- sum(titanic_cleaned$Survived == 1)
+print(totalSurvived)
+
+genderCount <- titanic_cleaned %>%
+  group_by(Sex)
+  summarise(Total = n())
+
